acddfd25732e3844077d5ecfd13481ba03f73021
annalenag <159128053+annalenag@users.noreply.github.com>
2024-08-23 15:00:55 +0200

    Added data collection profiles.
    Added data collection profiles (some of the defined in the MagnerResponsePowershell script) that can be selected when executing script with a "-Profile" parameter without need to manually edit the script. Default "triage" profile collects RAM, volatile information, system files and extended process information.

--- Defender_RESPONSE.ps1
+++ Defender_RESPONSE.ps1
@@ -9,12 +9,27 @@ This script can be used to leverage Magnet RESPONSE and the Microsoft Defender f
 
 Prerequisites:
 - Defender Live Response Console - upload MagnetRESPONSE.exe to the Library
-- Defender Live Response Console - upload Defender_RESPONSE.ps1 to the Library
+- Defender Live Response Console - upload Defender_RESPONSE.ps1 to the Library (to use Profiles enable "Script parametes")
+
+Collection Profiles:
+Included some of the profiles defined in MagnetResponsePowerShell script. It enables user to choose what information to collect without modifying the script.
+To choose the profile, run script with "-Profile" parameter and profile name value.
+If profile parameter is not provided during execution, default traige (ram, volatile, system files, extended process info) information will be collected.
+
+Available profiles names:
+- ram: only RAM
+- volatile: RAM, Volatile
+- triage: RAM, Volatile, system Files, Extended process info
+- full: RAM, Page file, System files, Extended process info, Proc files
+
 
 Operation:
 1. 'connect' to endpoint in Live Response // establish connection with the endpoint
 2. 'put MagnetRESPONSE.exe' // copies the exe to the target system
 3. 'run Defender_RESPONSE.ps1' // where the magic happens
+
+Alternative to 3. Choose profile.
+3.alt. 'run Defender_RESPONSE.ps1 -parameters "-Profile <profile_name>"'
 
 Retrieving the Data:
 
@@ -30,10 +45,38 @@ filename.zip
 > &
 
 #>
+param (
+    [string]$Profile="triage"
+)
+
 Write-Host ""
 Write-Host  "Magnet RESPONSE v1.7
 $([char]0x00A9)2021-2023 Magnet Forensics, LLC
 "
+
+switch($Profile){
+    "ram" {
+        $profile_info = ": RAM";
+        $arguments = "/captureram"
+    }
+    "volatile" {
+        $profile_info = ": RAM, Volatile"; 
+        $arguments = "/captureram /capturevolatile"
+    }
+    "triage" {
+        $profile_info = ": RAM, Volatile, System Files, Extended process info";
+        $arguments = "/captureram /capturevolatile /capturesystemfiles /captureextendedprocessinfo"
+    }
+    "full" {
+        $profile_info = ": Full collection";
+        $arguments = "/captureram /capturepagefile /capturevolatile /capturesystemfiles /captureextendedprocessinfo /saveprocfiles"
+    }
+    Default {
+        "Unkown profile. Available profile names: ram, volatile, systemfiles, triage, full.";
+        Exit
+    }
+}
+
 $OS = $(((gcim Win32_OperatingSystem -ComputerName $server.Name).Name).split('|')[0])
 $arch = (get-wmiobject win32_operatingsystem).osarchitecture
 $name = (get-wmiobject win32_operatingsystem).csname
@@ -43,10 +86,12 @@ Hostname: $name
 Operating System: $OS
 Architecture: $arch
 "
-./MagnetRESPONSE.exe /accepteula /unattended /output:C:\temp\RESPONSE /caseref:DefenderRESPONSE /capturevolatile /capturesystemfiles
+
+./MagnetRESPONSE.exe /accepteula /unattended /output:C:\temp\RESPONSE /caseref:DefenderRESPONSE $arguments
 # To include RAM capture, comment out (#) the above line and un-comment the line below
+# When enabled it will ignore profile parameters
 # ./MagnetRESPONSE.exe /accepteula /unattended /output:C:\temp\RESPONSE /caseref:DefenderRESPONSE /capturevolatile /capturesystemfiles /captureram
-Write-Host  "[Collecting Arifacts]"
+Write-Host  "[Collecting Arifacts$profile_info]"
 Wait-Process -name "MagnetRESPONSE"
 $null = $stopwatch.Elapsed
 $Minutes = $StopWatch.Elapsed.Minutes
